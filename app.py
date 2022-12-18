ac75473e304650ef153caec78afab9e08e3607a7
rajaamirapu <rajaamirapu@gmail.com>
2022-12-18 12:11:08 +0530

    first commit

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ from jabebot import ask, append_interaction_to_chat_log
 app = Flask(__name__)
 # if for some reason your conversation with Jabe gets weird, change the secret key
 app.config['SECRET_KEY'] = 'any-random-string'
-@app.route('/jabebot’, methods=['POST'])
+@app.route('/jabebot', methods=['POST'])
 def jabe():
  incoming_msg = request.values['Body']
- chat_log = session.get('chat_log’)
+ chat_log = session.get('chat_log')
  answer = ask(incoming_msg, chat_log)
- session['chat_log’] = append_interaction_to_chat_log(incoming_msg, answer,
+ session['chat_log'] = append_interaction_to_chat_log(incoming_msg, answer,
  chat_log)
  msg = MessagingResponse()
  msg.message(answer)
  return str(msg)
-if __name__ == '__main__’:
+if __name__ == '__main__':
  app.run(debug=True)
